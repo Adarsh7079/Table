66c4ffea00488ff195e8ea13aa6fa49308055b84
Adarsh7079 <adarshk8271@gmail.com>
2024-09-27 01:04:18 +0530

    sixth

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,22 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import * as React from "react"
 
-// Utility function for conditional classnames (if you don't have cn in @/lib/utils)
+// Utility function for conditional classnames
 function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+// ForwardRef Input component
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, type = "text", ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-         
+       
         )}
         ref={ref}
         {...props}
