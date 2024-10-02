c10aaf349556d2bf6cf2b58c681c0380ef906b78
adarsh7079 <adarshk8271@gmail.com>
2024-10-03 01:01:03 +0530

    error_fix

--- src/app/(form)/item/page.tsx
+++ src/app/(form)/item/page.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { number, z } from "zod";
+import {  z } from "zod";
 
 import { Button } from "@/components/ui/button";
 
